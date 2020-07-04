8ab4cabbcb882b78a33132eb2cc8838c74085808
D33NCRYP73D <deencrypted1@gmail.com>
2020-07-04 17:38:05 +0200

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -3,11 +3,11 @@ from cryptography import fernet
 
 key = fernet.Fernet.generate_key()
 
-with open("configs/enc.key", "w") as f:
+with open("configs/enc.key", "wb") as f:
     f.write(key)
 
 with open("configs/enc.key", "rb") as f:
     enc = fernet.Fernet(f.read())
 
 token = input("Whats your token?\n> ").encode()
-print("Store this in the startup.json: ", enc.encrypt(token))
+print("Store this in the startup.json: ", enc.encrypt(token).decode())
