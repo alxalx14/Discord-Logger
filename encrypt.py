0044d3711e9ccde65fe942788290efaacbc0f5e7
D33NCRYP73D <deencrypted1@gmail.com>
2020-07-04 17:53:36 +0200

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,13 +1,17 @@
 from cryptography import fernet
+import json
 
 
 key = fernet.Fernet.generate_key()
-
 with open("configs/enc.key", "wb") as f:
     f.write(key)
+    
+with open("configs/enc.key", "rb") as enc_file:
+    enc = fernet.Fernet(enc_file.read())
 
-with open("configs/enc.key", "rb") as f:
-    enc = fernet.Fernet(f.read())
+token = enc.encrypt(input("Whats your token?\n> ").encode()).decode()
+with open("configs/startup.json", "w") as config_file:
+    config_file.write(json.dumps({"token": token}))
 
-token = input("Whats your token?\n> ").encode()
-print("Store this in the startup.json: ", enc.encrypt(token).decode())
+print("""If you do not see a logs folder, run setup.py as it will install
+        all required modules and setup the log structure""")
